3dab65af64d11967468604d3bf05fa226280667e
Rob Burley <rcburley@icloud.com>
2019-04-25 22:57:00 +0100

    Import reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk'
+import hotelStoreConfig from './reducers/hotels';
 
 const createReducer = (initialState, handlers) => {
     return (state = initialState, action) => {
