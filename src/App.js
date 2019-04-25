96105577e3b775484dc06ce4e5340635551dcbf7
Rob Burley <rcburley@icloud.com>
2019-04-25 22:46:46 +0100

    Update App component to use redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {connect} from 'react-redux';
 
-function App() {
-    return (
-        <div>
-            <nav className="navbar navbar-dark bg-primary">
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-primary">
                     <span className="navbar-brand">
                         Rob Burley
                     </span>
-            </nav>
+                </nav>
 
-            <div className="container mt-5">
-                Components here
+                <div className="container mt-5">
+                    Components here
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default App;
+export default connect(state => state)(App)
