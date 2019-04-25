4539bb301a46c6ea474da39bf0bf281db492ef1d
Rob Burley <rcburley@icloud.com>
2019-04-26 00:15:46 +0100

    Add actions for updating filters

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -3,7 +3,38 @@ const initialState = {
     facilities: []
 };
 
-const actions = {};
+const actions = {
+    'UPDATE_STARS': updateStars,
+    'TOGGLE_FACILITIES': toggleFacilities,
+};
+
+
+function updateStars(state, action) {
+    return {
+        ...state,
+        stars: action.payload,
+    }
+}
+
+function toggleFacilities(state, action) {
+    if (state.facilities.includes(action.payload)) {
+        return {
+            ...state,
+            facilities: state.facilities.filter((f) => {
+                return f !== action.payload
+            })
+        }
+    } else {
+        state.facilities.push(action.payload)
+
+        return {
+            ...state,
+            facilities: state.facilities
+        }
+    }
+
+
+}
 
 export default {
     initialState,
